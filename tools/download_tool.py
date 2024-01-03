294d8ce1292804782a5765862fe6626516779668
xiaolxl <481719882@qq.com>
2024-01-03 14:26:14 +0800

    更新

--- tools/download_tool.py
+++ tools/download_tool.py
@@ -1,17 +1,17 @@
 import os
 
-def get_download_command(download_link,download_path,filename,download_method,download_type="url"):
+def get_download_command(download_link, download_path, filename, download_method, download_type="url"):
     command = ""
     if download_type == "url":
         if download_method == "more":
-            command = "cd " + download_path + " && aria2c -s 16 -x 8 --seed-time=0 '" + download_link + "' -o " + filename + " && echo 下载完毕!文件已保存到" + download_path
+            command = "mkdir -p " + download_path + " && cd " + download_path + " && aria2c -s 16 -x 8 --seed-time=0 '" + download_link + "' -o " + filename + " && echo 下载完毕!文件已保存到" + download_path
         if download_method == "one":
-            command = "cd " + download_path + " && aria2c --seed-time=0 '" + download_link + "' -o " + filename + " && echo 下载完毕!文件已保存到" + download_path
+            command = "mkdir -p " + download_path + " && cd " + download_path + " && aria2c --seed-time=0 '" + download_link + "' -o " + filename + " && echo 下载完毕!文件已保存到" + download_path
     
     if download_type == "cg":
         folder_name = download_link.split('/')[-2]  # 链接格式为 .../folder_name/filename
         # 构建下载命令
-        command = "cd " + download_path + " && "
+        command = "mkdir -p " + download_path + " && cd " + download_path + " && "
         # 根据下载方法添加下载命令
         command += "cg down " + download_link + " && "
         # 移动文件到正确位置
@@ -22,6 +22,7 @@ def get_download_command(download_link,download_path,filename,download_method,do
         command += "echo 下载完毕!文件已保存到:" + os.path.join(download_path, filename)
     
     return command
+
 
 def check_downloaded(filename, download_path):
         # 检查文件是否已下载
